41e5e69e0951ef2bc42e0ed752f6bc361e8a08cb
Sudip Biswas <sudipbiswas2142@gmail.com>
2024-08-03 02:01:15 +0530

    profile screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -3,17 +3,19 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, FlatList, Image, RefreshControl } from "react-native";
 
 import { images } from "../../constants";
+import { useGlobalContext } from "../../context/GlobalProvider";
 import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
-import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
-import EmptyState from "../../components/EmptyState";
 import useAppWrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
+import EmptyState from "../../components/EmptyState";
+import SearchInput from "../../components/SearchInput";
 
 const Home = () => {
   const [refreshing, setRefreshing] = useState(false);
   const { data: posts, isLoading, refetch } = useAppWrite(getAllPosts);
   const { data: latestPosts } = useAppWrite(getLatestPosts);
+  const { user, setIsLoggedIn, setUser } = useGlobalContext();
 
   const onRefresh = async () => {
     setRefreshing(true);
@@ -32,10 +34,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome Back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Sudip
+                  {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,82 @@
-import { View, Text } from "react-native";
+import { router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, FlatList, TouchableOpacity, Image } from "react-native";
+
+import { icons } from "../../constants";
+import { getUserPosts, signOut } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import InfoBox from "../../components/InfoBox";
+import useAppWrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
+import EmptyState from "../../components/EmptyState";
 
 const Profile = () => {
+  const { user, setIsLoggedIn, setUser } = useGlobalContext();
+  const { data: posts } = useAppWrite(() => getUserPosts(user?.$id));
+
+  const logout = async () => {
+    await signOut();
+    setUser(null);
+    setIsLoggedIn(false);
+
+    router.replace("/sign-in");
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity
+              className="w-full items-end mb-10"
+              onPress={logout}
+            >
+              <Image
+                source={icons.logout}
+                resizeMode="contain"
+                className="h-6 w-6"
+              />
+            </TouchableOpacity>
+
+            <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+              <Image
+                source={{ uri: user?.avatar }}
+                className="w-[90%] h-[90%] rounded-lg"
+                resizeMode="cover"
+              />
+            </View>
+            <InfoBox
+              title={user?.username}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
+            />
+
+            <View className="mt-5 flex-row">
+              <InfoBox
+                title={posts.length || 0}
+                subtitle="Posts"
+                containerStyles="mr-10"
+                titleStyles="text-xl"
+              />
+              <InfoBox
+                title="1.2k"
+                subtitle="Followers"
+                titleStyles="text-xl"
+              />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this search query"
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
