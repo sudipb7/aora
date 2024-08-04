93100a565e7b0047f37cfd6f4e5416cb14d1a65a
Sudip Biswas <sudipbiswas2142@gmail.com>
2024-08-04 20:54:01 +0530

    remove react-native-dotenv

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      "nativewind/babel",
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          verbose: false,
-          path: ".env",
-        },
-      ],
-    ],
+    plugins: ["nativewind/babel"],
   };
 };
